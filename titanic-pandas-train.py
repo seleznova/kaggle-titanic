260a9019abeecc153d7c356dfde09b481b4955b6
seleznova <mary.kuzmenko@gmail.com>
2016-11-24 08:17:44 +0000

    Pandas updates

--- titanic-pandas-train.py
+++ titanic-pandas-train.py
@@ -1,21 +1,6 @@
-import csv as csv
+import pandas as pd
 import numpy as np
 
-csv_file_object = csv.reader(open('train.csv', 'rb'))
-header = csv_file_object.next()
-data = []
+df = pd.read_csv('train.csv', header=0)
 
-for row in csv_file_object:
-    data.append(row)
-data = np.array(data)
-
-<<<<<<< HEAD
-print data[0:15, 5]
-
-type(data[0::,5])
-
-numpy.ndarray
-
-=======
-print data
->>>>>>> f3ab3901a6b77f2a785e3399ffc3388c71c196f6
+print df
